b6e52c46d95bc10421ed85a3e8a7d92f8b60a170
Christopher Conroy <chrishconroy@gmail.com>
2022-11-25 22:45:19 -0800

    Insert training functions

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 # Display live progress bar in console
 class ProgressBar:
 
-    def __init__(self, total_steps, bar_length=40):
+    def __init__(self, prefix, total_steps, bar_length=40):
         # Initialize
-        self.steps = 0
+        self.prefix = prefix
         self.total_steps = total_steps
         self.bar_length = bar_length
+        self.steps = 0
 
     def step(self):
         # Create progress bar
@@ -16,7 +17,7 @@ class ProgressBar:
         bar = '█' * fill_length + '-' * empty_length
 
         # Update progress bar display
-        output = 'Progress: |{}| {:.1f}%'.format(bar, percent * 100)
+        output = '{}: |{}| {:.1f}%'.format(self.prefix, bar, percent * 100)
         print(output, end='\r')
 
         # Print new line upon completion
